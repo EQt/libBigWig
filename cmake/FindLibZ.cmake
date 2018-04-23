1e5f9ea3f6c0c75eef2bb563b1e8cb7caee37e21
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-04-23 23:44:19 +0200

    wip

---
+++ cmake/FindLibZ.cmake
@@ -0,0 +1,44 @@
+find_package(ZLIB)
+
+if (NOT ZLIB_FOUND)
+  message(STATUS "compiling ZLIB myself")
+
+  set(LIBZ_INSTALL_DIR ${CMAKE_BINARY_DIR})
+  externalProject_Add(zlib
+    PREFIX  ${PROJECT_BINARY_DIR}/zlib
+    URL     http://zlib.net/zlib-1.2.8.tar.gz
+    URL_MD5 44d667c142d7cda120332623eab69f40
+    UPDATE_COMMAND ""
+    DOWNLOAD_DIR ${LIBZ_PREFIX}
+    SOURCE_DIR   ${LIBZ_PREFIX}/libz-1.2.8
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${LIBZ_INSTALL_DIR}
+               -DBUILD_TYPE=Release
+               -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+               -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+  )
+  # set_property(TARGET zlib PROPERTY FOLDER "extern")
+
+  if (MSVC)
+    set(ZLIBLIBNAME zlibstatic${POSTFIX}.lib)
+    set(zlib_shared ${LIBZ_INSTALL_DIR}/lib/zlib${POSTFIX}.lib)
+  else(MINGW)
+    set(ZLIBLIBNAME libzlibstatic.a)
+    set(zlib_shared ${LIBZ_INSTALL_DIR}/lib/libzlib.dll.a)
+  else()
+    set(ZLIBLIBNAME ${CMAKE_STATIC_LIBRARY_PREFIX}zlib${CMAKE_STATIC_LIBRARY_SUFFIX})
+    message(ERROR "FindLibZ not configured for this case")
+  endif()
+  set(ZLIB_INCLUDE_DIR ${LIBZ_INSTALL_DIR}/include)
+  set(ZLIB_LIBRARY     ${LIBZ_INSTALL_DIR}/lib/${ZLIBLIBNAME})
+
+  externalProject_Add_Step(zlib remove_shared_zlib
+    DEPENDEES install
+    COMMAND ${CMAKE_COMMAND} -E remove ${zlib_shared}
+    COMMENT "Removing shared zlib (because we want the static version)"
+  )
+  externalProject_Add_Step(zlib copy_static_zlib
+    DEPENDEES remove_shared_zlib
+    COMMAND ${CMAKE_COMMAND} -E copy ${ZLIB_LIBRARY} ${zlib_shared}
+    COMMENT "Rename zlib from ${ZLIB_SHARED} to ${zlib_shared}"
+  )
+endif()
--- cmake/generate.py
+++ cmake/generate.py
@@ -3,8 +3,10 @@ Generate/update CMakeLists.txt by simulating make
 """
 import subprocess as sp
 import re
-from six.moves.urllib import request
+import os
 import yaml
+from six.moves.urllib import request
+
 
 
 def raw_github(fname, owner="conda", repo="conda-recipes", branch="master"):
@@ -21,19 +23,30 @@ def zlib_extern(cm):
 
 
 if __name__ == '__main__':
+    os.chdir(os.path.join(os.path.dirname(__file__), '..'))
+
     make = sp.run("make --always-make --dry-run libBigWig.so".split(),
                   stdout=sp.PIPE).stdout.decode().split('\n')
+    assert len(make) > 1, make
     assert make.pop() == ''
     assert '-shared' in make[-1]
     sources = [s.split(' ')[-1] for s in make[:-1]]
 
-
     with open("CMakeLists.txt", "w") as cm:
-        print("project(bigwig)", file=cm)
-        print(file=cm)
-        print("add_library(BigWig SHARED", file=cm)
+        print("""project(bigwig)
+set(CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/cmake")
+
+find_package(LibZ)
+
+include_directories(${ZLIB_INCLUDE_DIR})
+
+add_library(BigWig SHARED""", file=cm)
         for s in sources:
             print("   ", s, file=cm)
         print(")", file=cm)
         print(file=cm)
+        print("target_link_libraries(BigWig ${ZLIB_LIBRARY})", file=cm)
+        print("if (TARGET zlib)", file=cm)
+        print("    add_dependencies(BigWig zlib)", file=cm)
+        print("endif()", file=cm)
         # zlib_extern(cm)
